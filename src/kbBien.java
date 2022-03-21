bd5c7bb660e9b75aa0f7e5ff73627ed9884a9516
Nguyen Long <nguyenlong232253@gmail.com>
2022-03-21 10:41:49 +0700

    nopbaitap

---
+++ src/kbBien.java
@@ -0,0 +1,16 @@
+public class kbBien {
+    public static void main(String[] args) {
+        int i =10;
+        float f = 20.5f;
+        boolean b = true;
+        double d = 20.5;
+        char c = 'a';
+        String s = "Hà Nội";
+
+        System.out.println("i = " + i + "\n" + "f = " + f);
+        System.out.println("d = " + d);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("s = " + s);
+    }
+}
